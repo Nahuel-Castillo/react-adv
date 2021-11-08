112b9f2c3377916fd250e177056c0859f0c12912
Nahuel Castillo <nahuelcastillo13@gmail.com>
2021-11-07 22:30:45 -0600

    Lazy pages added

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,45 +1,62 @@
 import {
   BrowserRouter as Router,
-  Switch,
+  Routes,
   Route,
-  NavLink
-} from 'react-router-dom';
+  NavLink,
+  Outlet,
+} from "react-router-dom";
+import { LazyPage1, LazyPage2, LazyPage3 } from "../01-lazyload/pages";
 
-import logo from '../logo.svg';
+import logo from "../logo.svg";
+
+const Layout = () => {
+  return (
+    <div className="main-layout">
+      <nav>
+        <img src={logo} alt="React Logo" />
+        <ul>
+          <li>
+            <NavLink
+              to="/"
+              className={({ isActive }) => (isActive ? "nav-active" : "")}
+            >
+              Lazy1
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="lazy2"
+              className={({ isActive }) => (isActive ? "nav-active" : "")}
+            >
+              Lazy2
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="lazy3"
+              className={({ isActive }) => (isActive ? "nav-active" : "")}
+            >
+              Lazy3
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+      <Outlet />
+    </div>
+  );
+};
 
 export const Navigation = () => {
   return (
     <Router>
-      <div className="main-layout">
-        <nav>
-            <img src={ logo } alt="React Logo" />
-          <ul>
-            <li>
-              <NavLink to="/" activeClassName="nav-active" exact>Home</NavLink>
-            </li>
-            <li>
-              <NavLink to="/about" activeClassName="nav-active" exact>About</NavLink>
-            </li>
-            <li>
-              <NavLink to="/users" activeClassName="nav-active" exact>Users</NavLink>
-            </li>
-          </ul>
-        </nav>
-
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
-        <Switch>
-          <Route path="/about">
-            <h1>About</h1>
-          </Route>
-          <Route path="/users">
-            <h1>Users</h1>
-          </Route>
-          <Route path="/">
-            <h1>Home</h1>
-          </Route>
-        </Switch>
-      </div>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<LazyPage1 />} />
+          <Route path="lazy2" element={<LazyPage2 />} />
+          <Route path="lazy3" element={<LazyPage3 />} />
+          {/* <Route path="*" element={<LazyPage1 />} /> */}
+        </Route>
+      </Routes>
     </Router>
   );
-}
+};
